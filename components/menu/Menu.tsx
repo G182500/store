609ff7ddd6dca9b6b1212980b20640be5d777e24
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2024-02-16 08:24:45 -0300

    refactor

---
+++ components/menu/Menu.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import logo from "@/assets/images/Spotify-Logo.png";
+import {
+  FaCompactDisc,
+  FaHeadphones,
+  FaStickyNote,
+  FaTshirt,
+  FaUser,
+} from "react-icons/fa";
+
+export default function Menu() {
+  return <div className="flex flex-col w-72">Menu</div>;
+}
+
+//https://www.npmjs.com/package/react-icons
+//https://react-icons.github.io/react-icons/icons/fa/
