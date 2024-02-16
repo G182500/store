a356734ea894c0babf68b36028c2962c7fca3b14
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2024-02-16 09:15:19 -0300

    feat: table

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from "@/components/table";
+
 export default function Home() {
-  return <main className="card">HOME</main>;
+  return (
+    <main className="card w-full">
+      <div className="rounded-md border">
+        <Table className="">
+          <TableBody>
+            <TableRow>
+              <TableCell>Teste</TableCell>
+              <TableCell>Teste2</TableCell>
+              <TableCell>Teste3</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </main>
+  );
 }
