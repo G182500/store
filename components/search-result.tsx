5ccab2d2d360de21827d5259adc6ced88d4b6da5
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2024-08-22 16:58:15 -0300

    refactor: result page

--- components/search-result.tsx
+++ components/search-result.tsx
@@ -28,35 +28,48 @@ const Search = ({ children }: SearchResultProps) => {
 
   return (
     <>
-      {searchText ? (
+      {!searchText ? (
+        children
+      ) : (
         <div className="flex flex-col bg-[#1d1d1d] p-4 gap-4 w-full sm:rounded-lg">
           <div className="flex items-center justify-between">
             <p className="font-semibold text-xl md:text-2xl">Search Results</p>
-            {searchText && (
-              <Link
-                href="/"
-                className="flex font-semibold items-center bg-orange-600 gap-1 text-sm rounded-md p-1.5"
-                onClick={() => params.delete("search")}
-              >
-                <X size={22} />
-                {searchText}
-              </Link>
-            )}
+            <Link
+              href="/"
+              className="flex font-semibold items-center bg-orange-600 gap-1 text-sm rounded-md p-1.5"
+              onClick={() => params.delete("search")}
+            >
+              <X size={22} />
+              {searchText}
+            </Link>
           </div>
-          {products && products.length ? (
-            <div className="flex flex-col items-center justify-center space-y-2">
-              {products.map((product) => (
-                <ProductCard product={product} key={product._id} />
-              ))}
+          {getProducts.isFetching ? (
+            <div className="flex gap-2 justify-center w-full">
+              <Loader2
+                color="#4ade80"
+                strokeWidth={3}
+                className="animate-spin"
+              />
+              <p className="font-medium opacity-80 text-xl text-white">
+                Buscando produtos...
+              </p>
             </div>
           ) : (
-            <p className="font-medium opacity-80 text-xl text-white self-center">
-              {getProducts.data?.message}
-            </p>
+            <>
+              {products?.length ? (
+                <div className="flex flex-col items-center justify-center space-y-2">
+                  {products.map((product) => (
+                    <ProductCard product={product} key={product._id} />
+                  ))}
+                </div>
+              ) : (
+                <p className="font-medium opacity-80 text-xl text-white self-center">
+                  {getProducts.data?.message}
+                </p>
+              )}
+            </>
           )}
         </div>
-      ) : (
-        children
       )}
     </>
   );
