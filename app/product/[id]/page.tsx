b02154901a695eaaf768a6d5b67c14e972328f56
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2024-08-28 12:08:20 -0300

    refactor: title and price in about product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -2,7 +2,9 @@
 import Image from "next/image";
 import { ImageIcon, Loader2 } from "lucide-react";
 import { useGetProductById } from "@/services/product/use-get-by-id";
+import formatToCurrency from "@/utils/format-currency";
 import ContentArea from "@/components/content-area";
+import Skeleton from "@/components/skeleton";
 
 export default function ProductDetail({ params }: { params: { id: string } }) {
   const getProduct = useGetProductById(params.id, {
@@ -14,31 +16,42 @@ export default function ProductDetail({ params }: { params: { id: string } }) {
   //Futuramente um serviço como Google Cloud Service, para salvar as imagens
   const coverImage = product?.images_url;
 
-  const imgSize = "h-80 w-80 border-2 border-[#4e4e4e] rounded-lg";
+  const imgSize = "h-80 w-80 rounded-lg";
 
   const header = (
     <>
       <p className="font-semibold text-xl md:text-2xl">Product Detail</p>
-      <span className="font-semibold bg-orange-600 h-fit text-sm rounded-md p-1.5">
-        {product?.category}
-      </span>
+      {product?.category && (
+        <span className="font-semibold bg-orange-600 text-sm rounded-md p-1.5">
+          {product?.category}
+        </span>
+      )}
     </>
   );
 
   return (
     <ContentArea header={header}>
-      <div className="flex flex-col items-center justify-center space-y-2">
+      <div className="flex flex-col items-center justify-center md:flex-row">
         {!product || getProduct.isFetching ? (
-          <div className="flex h-28 items-center justify-center space-x-2 w-full">
-            <Loader2 color="#4ade80" strokeWidth={3} className="animate-spin" />
-            <p className="font-medium opacity-80 text-xl text-white">
-              Carregando produto...
-            </p>
-          </div>
+          <>
+            <Skeleton className={`bg-[#4e4e4e] animate-pulse ${imgSize}`} />
+            <div className="flex h-28 items-center justify-center space-x-2 w-full">
+              <Loader2
+                color="#4ade80"
+                strokeWidth={3}
+                className="animate-spin"
+              />
+              <p className="font-medium opacity-80 text-lg text-white">
+                Carregando dados do produto...
+              </p>
+            </div>
+          </>
         ) : (
           <>
             {coverImage ? (
-              <div className={`relative self-center ${imgSize}`}>
+              <div
+                className={`relative self-center border-2 border-[#4e4e4e] ${imgSize}`}
+              >
                 <Image
                   fill
                   src={`/imgs/products/${coverImage}`}
@@ -48,7 +61,7 @@ export default function ProductDetail({ params }: { params: { id: string } }) {
               </div>
             ) : (
               <div
-                className={`flex flex-col items-center justify-center ${imgSize}`}
+                className={`flex flex-col items-center justify-center border-2 border-[#4e4e4e] ${imgSize}`}
               >
                 <ImageIcon size={200} color="#9c9c9c" />
                 <p className="font-medium text-xl text-[#9c9c9c]">
@@ -56,14 +69,19 @@ export default function ProductDetail({ params }: { params: { id: string } }) {
                 </p>
               </div>
             )}
-            <p className="font-medium text-center opacity-80 text-xl text-white">
-              {product?.title} AAAAAAA AAAAAA AAAAA
-            </p>
-            <div className="flex items-center justify-between w-full">
-              <p className="font-semibold text-2xl text-green-400">
-                R$ {product?.price}00
+            <div className="grid grid-cols-3 space-x-2 mt-4 w-full">
+              <p className="font-medium col-span-2 text-left opacity-80 text-xl text-white">
+                {product?.title}
               </p>
-              <p>Disponíveis: {product?.quantity}</p>
+              <p className="font-semibold text-right text-2xl text-green-400">
+                R$ {formatToCurrency(product?.price)}
+              </p>
+            </div>
+            <div className="flex justify-between w-full">
+              <p>AAA</p>
+              <p className="font-semibold italic text-sm">
+                Disponíveis: {product?.quantity}
+              </p>
             </div>
           </>
         )}
